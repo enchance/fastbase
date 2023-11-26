444c2d41d2d150d4ddaee828258bdf864f74c69e
enchance <enchance@gmail.com>
2023-11-26 15:02:42 +0800

    feat: Group.create, Group.append

--- fastbase/globals.py
+++ fastbase/globals.py
@@ -2,6 +2,12 @@ import pytz, logging
 from fastapi.logger import logger
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
+from icecream import IceCreamDebugger
+
+
+
+ic = IceCreamDebugger()
+# ic.disable()
 
 
 # Logs
--- fastbase/models/Group.py
+++ fastbase/models/Group.py
@@ -1,9 +1,13 @@
+from typing import Self
 from sqlmodel import SQLModel, Field, String
 from sqlalchemy import Column
+from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.exc import IntegrityError
 
 from .mixins import IntPK
 from ..utils import modstr
+from fastbase import ic
 
 
 class Group(IntPK, SQLModel, table=True):
@@ -14,3 +18,27 @@ class Group(IntPK, SQLModel, table=True):
 
     def __str__(self):
         return modstr(self, 'name')
+
+    # TESTME: Untested
+    @classmethod
+    async def create(cls, session: AsyncSession, name: str,  permissions: set,
+                     description: str | None = None) ->Self:
+        """Create a new group"""
+        try:
+            group = cls(name=name, permissions=permissions, description=description)
+            session.add(group)
+            await session.commit()
+            await session.refresh(group)
+            return group
+        except IntegrityError:
+            raise
+
+    # TESTME: Untested
+    @classmethod
+    async def append(cls, session: AsyncSession, *, id: int, permissions: set) -> Self:
+        """Append new permissions to group"""
+        if group := await session.get(cls, id):
+            group.permissions = {*group.permissions, *permissions}
+            session.add(group)
+            await session.commit()
+        return group
