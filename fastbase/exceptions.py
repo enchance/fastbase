50cf047db04d22a3edbb3cd8bbafb85e6a36699c
enchance <enchance@gmail.com>
2023-12-16 13:38:22 +0800

    add key to exceptions

--- fastbase/exceptions.py
+++ fastbase/exceptions.py
@@ -1,41 +1,43 @@
-from typing import Optional, Any, Dict
+from typing import Optional, Any, Dict, AnyStr, TypeVar
 from fastapi.exceptions import HTTPException
+
+
+KEY = str | int | float | bool
 
 
 class AppException(HTTPException):
     message = 'FAILED_ATTEMPT'
     code = 400
 
-    def __init__(self, message: Any = None, status_code: int = None,
-                 headers: Optional[Dict[str, Any]] = None):
+    def __init__(self, message: Any | None = None, *, status_code: int | None = None, key: KEY | None = None,
+                 headers: dict[str, Any] | None = None,):
         message = message or self.message
+        if key:
+            message = f'{message} [{key}]'
         status_code = status_code or self.code
         super().__init__(status_code=status_code, detail=message, headers=headers)
 
 
 class PermissionsException(AppException):
     code = 403
-    message = 'YOU_SHALL_NOT_PASS'
+    message = 'YOU_SHALL_NOT_PASS: Insufficient permissions'
 
 
 class InvalidToken(AppException):
     code = 403
-    message = 'INVALID_TOKEN'
+    message = 'INVALID_TOKEN: Your token is bad and you should feel bad (is it expired?)'
 
 
 class AuthError(AppException):
     code = 401
-    message = 'ACCOUNT_AUTHENTICATION'
+    message = 'ACCOUNT_AUTHENTICATION: Unable to authenticate the user'
 
 
 class UserNotFoundError(AppException):
     code = 404
-    message = 'USER_NOT_FOUND'
+    message = 'USER_NOT_FOUND: That user doesn\'t exist'
 
 
-class CallbackError(Exception):
-    def __init__(self, message: str = '', *, name: str = ''):
-        message = message or 'CALLBACK_FAILED'
-        if name:
-            message += f' {name}'
-        raise Exception(message)
+class CallbackError(AppException):
+    code = 500
+    message = 'CALLBACK_ERROR: You heard me'
