ac9a99d700b53512de0c7c295584d42e5fd0266d
enchance <enchance@gmail.com>
2023-12-29 18:41:13 +0800

    create UserMod.detach_permission()

--- fastbase/models/UserMod.py
+++ fastbase/models/UserMod.py
@@ -157,80 +157,75 @@ class UserMod(DTMixin, UuidPK, SQLModel):
         if async_callback:
             await async_callback(recipient.email, list(groups))
 
-    # # TESTME: Untested
-    # async def attach_permission(self, session: AsyncSession, recipient: Self, perm: str,
-    #                             *, caching: Callable[[str, list], None] | None = None,
-    #                             async_callback: Callable[[str, list], Awaitable[None]] | None = None):
-    #     """
-    #     Attach a permission to user. Prevents duplicates.
-    #     :param session:     AsyncSession
-    #     :param recipient:   The user to recieve the group
-    #     :param perm:        Permission name
-    #     :param caching:     Callback for caching data
-    #     :param async_callback:    Async callback for generic use
-    #     :raises PermissionsException:
-    #     :return:            None
-    #     """
-    #     def _attach(perm_: str) -> list[str]:
-    #         neg_ = f'-{perm_}'
-    #         permissions_ = set(recipient.permissions)
-    #
-    #         ic(permissions_)
-    #         if neg_ in permissions_:
-    #             permissions_.remove(neg_)
-    #         elif perm_ not in permissions_:
-    #             permissions_.add(perm_)
-    #         ic(permissions_)
-    #         return list(permissions_)
-    #
-    #     if not await self.has('permission.attach'):
-    #         raise PermissionsException()
-    #
-    #     # ic(recipient.permissions)
-    #     permissions = _attach(perm)
-    #     recipient.permissions = permissions
-    #     await session.commit()
-    #     # ic(recipient.permissions)
-    #
-    #     if caching:
-    #         caching(recipient.email, permissions)
-    #     if async_callback:
-    #         await async_callback(recipient.email, permissions)
-    #
-    # # TESTME: Untested
-    # async def detach_permission(self, session: AsyncSession, recipient: Self, perm: str,
-    #                             *, caching: Callable[[str, list], None] | None = None,
-    #                             async_callback: Callable[[str, list], Awaitable[None]] | None = None):
-    #     """
-    #     Remove a permission from user. If the perm is a starter perm (can't be removed) then a negation is added.
-    #     :param session:     AsyncSession
-    #     :param recipient:   The user who's group is to be removed
-    #     :param perm:        Permission name
-    #     :param caching:     Callback for caching data
-    #     :param async_callback:    Async callback for generic use
-    #     :raises PermissionsException:
-    #     :return:            None
-    #     """
-    #     def _detach(name_: str) -> list[str]:
-    #         neg_ = f'-{name_}'
-    #         groups_ = set(recipient.groups)
-    #
-    #         if name_ in groups_:
-    #             groups_.remove(name_)
-    #         else:
-    #             groups_.add(neg_)
-    #         return list(groups_)
-    #
-    #     if not await self.has('permission.detach'):
-    #         raise PermissionsException()
-    #
-    #     groups = _detach(perm)
-    #     recipient.groups = groups
-    #     await session.commit()
-    #
-    #     if caching:
-    #         caching(recipient.email, list(groups))
-    #     if async_callback:
-    #         await async_callback(recipient.email, list(groups))
+    async def attach_permission(self, session: AsyncSession, recipient: Self, perm: str,
+                                *, caching: Callable[[str, list], None] | None = None,
+                                async_callback: Callable[[str, list], Awaitable[None]] | None = None):
+        """
+        Attach a permission to user. Prevents duplicates.
+        :param session:     AsyncSession
+        :param recipient:   The user to recieve the group
+        :param perm:        Permission
+        :param caching:     Callback for caching data
+        :param async_callback:    Async callback for generic use
+        :raises PermissionsException:
+        :return:            None
+        """
+        # async with AsyncSession(async_engine) as sess:
+        # async with asynccontextmanager(get_session)() as sess:
+        #     user = await User.get_by_email(sess, 'admin@gmail.com', skip_cache=True)
+        def _attach(name_: str) -> list[str]:
+            neg_ = f'-{name_}'
+            perms_ = set(recipient.permissions)
 
+            if neg_ in perms_:
+                perms_.remove(neg_)
+            elif name_ not in perms_:
+                perms_.add(name_)
+            return list(perms_)
 
+        if not await self.has('permission.attach'):
+            raise PermissionsException()
+
+        permissions = _attach(perm)
+        recipient.permissions = permissions
+        await session.commit()
+
+        if caching:
+            caching(recipient.email, permissions)
+        if async_callback:
+            await async_callback(recipient.email, permissions)
+
+    async def detach_permission(self, session: AsyncSession, recipient: Self, perm: str,
+                                *, caching: Callable[[str, list], None] | None = None,
+                                async_callback: Callable[[str, list], Awaitable[None]] | None = None):
+        """
+        Remove a permission from user. If the permission is a starter group (can't be removed) then a negation is added.
+        :param session:     AsyncSession
+        :param recipient:   The user who's group is to be removed
+        :param perm:        Permission
+        :param caching:     Callback for caching data
+        :param async_callback:    Async callback for generic use
+        :raises PermissionsException:
+        :return:            None
+        """
+        def _detach(perm_: str) -> list[str]:
+            neg_ = f'-{perm_}'
+            perms_ = set(recipient.permissions)
+
+            if perm_ in perms_:
+                perms_.remove(perm_)
+            else:
+                perms_.add(neg_)
+            return list(perms_)
+
+        if not await self.has('permission.detach'):
+            raise PermissionsException()
+
+        permissions = _detach(perm)
+        recipient.permissions = permissions
+        await session.commit()
+
+        if caching:
+            caching(recipient.email, list(permissions))
+        if async_callback:
+            await async_callback(recipient.email, list(permissions))
